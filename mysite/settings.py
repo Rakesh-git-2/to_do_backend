3cd476b3328b5201a19b4b75382f43381f2907a0
rakesh <rakesh@fctrz.in>
2023-12-16 20:45:46 +0000

    allow any fix

--- mysite/settings.py
+++ mysite/settings.py
@@ -134,9 +134,11 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 REST_FRAMEWORK = {  
     'DEFAULT_PERMISSION_CLASSES': [     
         'rest_framework.permissions.IsAuthenticated', 
+        
     ], 
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication', 
+        'rest_framework.permissions.AllowAny'
     ], 
 } 
 
